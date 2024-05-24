1f7a0f9f7de320777b6b9b8a3da836d879ec5a68
MARCO ANTONIO FEITOSA GOMES DE MELO <marco.melo@a.catolica-to.edu.br>
2024-05-24 17:24:25 -0300

    adcionando mais cores no arquivo de cores

--- cores.py
+++ cores.py
@@ -10,9 +10,27 @@ class colors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     END = '\033[0m'
+    # Cores adicionais
+    BLACK = '\033[30m'
+    WHITE = '\033[37m'
+    MAGENTA = '\033[35m'
+    ORANGE = '\033[33m'
+    GRAY = '\033[90m'
+    LIGHTGRAY = '\033[37m'
+    LIGHTBLUE = '\033[94m'
+    LIGHTGREEN = '\033[92m'
+    LIGHTCYAN = '\033[96m'
+    LIGHTRED = '\033[91m'
+    LIGHTPURPLE = '\033[95m'
 
-# # Usa as cores definidas
-# print(colors.RED + 'Este texto será vermelho!' + colors.END)
-# print(colors.BOLD + 'Este texto será em negrito!' + colors.END)
-# print(colors.UNDERLINE + 'Este texto será sublinhado!' + colors.END)
-# print(colors.GREEN + 'Este texto será verde!' + colors.END)
+# Usa as cores definidas
+"""print(colors.RED + 'Este texto será vermelho!' + colors.END)
+print(colors.BOLD + 'Este texto será em negrito!' + colors.END)
+print(colors.UNDERLINE + 'Este texto será sublinhado!' + colors.END)
+print(colors.GREEN + 'Este texto será verde!' + colors.END)
+print(colors.LIGHTBLUE + 'Este texto será azul claro!' + colors.END)
+print(colors.LIGHTGREEN + 'Este texto será verde claro!' + colors.END)
+print(colors.LIGHTRED + 'Este texto será vermelho claro!' + colors.END)
+print(colors.LIGHTPURPLE + 'Este texto será roxo claro!' + colors.END)
+print(colors.LIGHTCYAN + 'Este texto será ciano claro!' + colors.END)
+"""
