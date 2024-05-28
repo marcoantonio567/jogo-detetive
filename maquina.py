eafcab1624a001f3e1de84d2338952d0b5de3b74
EVILLE VITÓRIA NUNES COELHO <eville.coelho@a.catolica-to.edu.br>
2024-05-28 20:02:09 -0300

    arrumando menu

--- maquina.py
+++ maquina.py
@@ -1,7 +1,7 @@
 import time
 import sys
 
-def type_writer(text, delay=0.0005):
+def type_writer(text, delay=0.01):
     for char in text:
         sys.stdout.write(char)
         sys.stdout.flush()
